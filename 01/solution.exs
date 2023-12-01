47f2fae7c6cd8b210167eb2671dc00447109b392
Alex Pearwin <alex@pearwin.com>
2023-12-01 22:38:22 +0000

    Day 1 part 2.

--- 01/solution.exs
+++ 01/solution.exs
@@ -1,14 +1,37 @@
 defmodule Solution do
-  @digits 0..9 |> Enum.map(fn i -> "#{i}" end) |> MapSet.new()
+  @digit_words %{
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9"
+  }
+  @digits @digit_words |> Map.values() |> MapSet.new()
 
   def part1 do
+    input()
+    |> Enum.map(&extract_digits/1)
+    |> Enum.map(&parse_digits/1)
+    |> Enum.map(&combine_digits/1)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    input()
+    |> Enum.map(&extract_digits_and_words/1)
+    |> Enum.map(&parse_digits/1)
+    |> Enum.map(&combine_digits/1)
+    |> Enum.sum()
+  end
+
+  defp input do
     File.read!("01/input.txt")
     |> String.trim()
     |> String.split("\n")
-    |> Enum.map(&extract_digits/1)
-    |> Enum.map(&combine_digits/1)
-    |> Enum.map(&parse_integer/1)
-    |> Enum.sum()
   end
 
   defp extract_digits(line) do
@@ -17,17 +40,37 @@ defmodule Solution do
     |> Enum.filter(fn character -> MapSet.member?(@digits, character) end)
   end
 
+  defp extract_digits_and_words(line) do
+    matches =
+      Enum.join(Enum.concat(Map.keys(@digit_words), Map.values(@digit_words)), "|")
+
+    # Use a look-ahead assertion to find overlapping matches.
+    # https://stackoverflow.com/a/11430936
+    ~r/(?=(#{matches}))/
+    |> Regex.scan(line)
+    |> Enum.concat()
+    |> Enum.filter(fn s -> s != "" end)
+  end
+
   defp combine_digits(digits) do
     first = List.first(digits)
     last = List.last(digits)
-    first <> last
+    10 * first + last
   end
 
-  defp parse_integer(s) do
-    {integer, _} = Integer.parse(s)
+  defp parse_digits(digits) do
+    Enum.map(digits, &parse_digit/1)
+  end
+
+  defp parse_digit(s) do
+    digit = Map.get(@digit_words, s, s)
+    {integer, _} = Integer.parse(digit)
     integer
   end
 end
 
 part1 = Solution.part1()
 IO.puts("Part 1: #{part1}")
+
+part2 = Solution.part2()
+IO.puts("Part 2: #{part2}")
