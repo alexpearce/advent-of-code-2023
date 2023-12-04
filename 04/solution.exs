bd98537a0fb3904e08d38094143a41ed66c29582
Alex Pearwin <alex@pearwin.com>
2023-12-04 09:54:03 +0000

    Day 4 part 2.

--- 04/solution.exs
+++ 04/solution.exs
@@ -4,14 +4,18 @@ defmodule Solution do
   def part1 do
     input()
     |> Enum.map(&parse/1)
-    |> Enum.map(&intersection/1)
+    |> Enum.map(&intersection_size/1)
     |> Enum.map(&tally/1)
     |> Enum.sum()
   end
 
   def part2 do
     input()
-    nil
+    |> Enum.map(&parse/1)
+    |> Enum.map(&intersection_size/1)
+    |> add_won_scratchcards()
+    |> Enum.map(fn {_key, value} -> value end)
+    |> Enum.sum()
   end
 
   defp input do
@@ -31,17 +35,32 @@ defmodule Solution do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp intersection({winning, held}) do
+  defp intersection_size({winning, held}) do
     MapSet.intersection(
       MapSet.new(winning),
       MapSet.new(held)
     )
+    |> MapSet.size()
   end
 
-  defp tally(intersection) do
-    count = MapSet.size(intersection)
+  defp tally(0), do: 0
 
-    if count == 0, do: 0, else: 2 ** (count - 1)
+  defp tally(intersection_size), do: 2 ** (intersection_size - 1)
+
+  defp add_won_scratchcards(intersection_sizes) do
+    intersection_sizes
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {card_intersection_size, index}, acc ->
+      acc = Map.put_new(acc, index, 1)
+
+      if card_intersection_size > 0 do
+        Enum.reduce(1..card_intersection_size, acc, fn offset, acc ->
+          Map.update(acc, index + offset, 1 + acc[index], fn value -> value + acc[index] end)
+        end)
+      else
+        acc
+      end
+    end)
   end
 end
 
