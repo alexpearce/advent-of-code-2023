a3f34909487b52f4b298cfcb8955337182ea973b
Alex Pearwin <alex@pearwin.com>
2023-12-07 11:36:16 +0000

    Day 7 part 2.

--- 07/solution.exs
+++ 07/solution.exs
@@ -1,11 +1,12 @@
 defmodule Solution do
   @cards ~W(A K Q J T 9 8 7 6 5 4 3 2)
-  @card_scores @cards |> Enum.reverse() |> Enum.with_index() |> Enum.into(%{})
+  @card_scores @cards |> Enum.reverse() |> Enum.with_index(1) |> Enum.into(%{})
+  @card_scores_with_joker_rule Map.put(@card_scores, "J", 0)
 
   def part1 do
     rounds = input()
     {hands, _bids} = Enum.unzip(rounds)
-    hand_ranks = rank_hands(hands)
+    hand_ranks = rank_hands(hands, @card_scores, false)
 
     rounds
     |> winnings(hand_ranks)
@@ -13,8 +14,13 @@ defmodule Solution do
   end
 
   def part2 do
-    input()
-    nil
+    rounds = input()
+    {hands, _bids} = Enum.unzip(rounds)
+    hand_ranks = rank_hands(hands, @card_scores_with_joker_rule, true)
+
+    rounds
+    |> winnings(hand_ranks)
+    |> Enum.sum()
   end
 
   defp input do
@@ -25,28 +31,32 @@ defmodule Solution do
     |> Enum.map(fn [hand, bid] -> {hand, String.to_integer(bid)} end)
   end
 
-  defp rank_hands(hands) do
+  defp rank_hands(hands, card_scores, joker_rule?) do
     hands
-    |> Enum.sort_by(&score_hand/1, :asc)
+    |> Enum.sort_by(&score_hand(&1, card_scores, joker_rule?), :asc)
     |> Enum.with_index(1)
     |> Enum.into(%{})
   end
 
-  defp score_hand(hand) do
+  defp score_hand(hand, card_scores, joker_rule?) do
     # A hand's score is the combination of its type and of its consituent cards.
-    card_scores = hand_to_card_scores(hand)
-    type_score = card_scores_to_type_score(card_scores)
+    card_scores = hand_to_card_scores(hand, card_scores)
+    type_score = card_scores_to_type_score(card_scores, joker_rule?)
     List.to_tuple([type_score | card_scores])
   end
 
-  defp hand_to_card_scores(hand) do
+  defp hand_to_card_scores(hand, card_scores) do
     hand
     |> String.graphemes()
-    |> Enum.map(fn card -> @card_scores[card] end)
+    |> Enum.map(fn card -> card_scores[card] end)
   end
 
-  defp card_scores_to_type_score(card_scores) do
-    freqs = Enum.frequencies(card_scores) |> Map.values() |> Enum.sort()
+  defp card_scores_to_type_score(card_scores, joker_rule?) do
+    freqs =
+      Enum.frequencies(card_scores)
+      |> find_best_hand(joker_rule?)
+      |> Map.values()
+      |> Enum.sort()
 
     case freqs do
       # Five of a kind.
@@ -63,6 +73,33 @@ defmodule Solution do
       [1, 1, 1, 2] -> 1
       # High card.
       [1, 1, 1, 1, 1] -> 0
+    end
+  end
+
+  defp find_best_hand(card_freqs, false = _joker_rule?), do: card_freqs
+
+  defp find_best_hand(card_freqs, true = _joker_rule?) do
+    case card_freqs[0] do
+      nil ->
+        # There are no jokers, so cannot do better.
+        card_freqs
+
+      5 ->
+        # There are no non-jokers, so cannot do better.
+        card_freqs
+
+      joker_freq ->
+        # Replace one joker with the most frequent non-joker card, then repeat.
+        {most_frequent_card, freq} =
+          card_freqs
+          |> Map.drop([0])
+          |> Enum.max_by(fn {_card, freq} -> freq end)
+
+        card_freqs
+        |> Map.put(0, joker_freq - 1)
+        |> Map.reject(fn {_k, v} -> v == 0 end)
+        |> Map.put(most_frequent_card, freq + 1)
+        |> find_best_hand(true)
     end
   end
 
